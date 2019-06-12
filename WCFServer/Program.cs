cf04de8aaffc6e1f0ebb6fe607aa11a2451291a5
Mattia <locastromattia@hotmail.it>
2019-06-12 12:01:24 +0200

    Server WCF + Ref. client services

---
+++ WCFServer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;//Liberia per fornire un Host di Servizi
+
+namespace WCFServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ServiceHost svcHost = new ServiceHost(typeof(ServerServices));      //Inizializzo un Server Host per i servizi implementati in "ServerServices"
+                svcHost.Open();     //Apre Il Server
+
+                Console.WriteLine("Server Attivo !!!");
+                Console.ReadLine();
+                svcHost.Close();
+                Console.WriteLine("Servizio Interrotto");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("UNTO: " + ex.ToString());
+            }
+        }
+    }
+}
---
+++ WCFServer/ServerServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFServer
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di classe "ServerServices" nel codice e nel file di configurazione contemporaneamente.
+    public class ServerServices : IServerServices
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
